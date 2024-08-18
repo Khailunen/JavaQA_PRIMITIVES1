42e58f2038945b0137570a9c750f3fa49ecd7d58
Karina Khaltunen <khaltunen@bk.ru>
2024-08-18 18:52:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tiket = 10_278;
+        int bonus = 20;
+
+        System.out.println ("За покупку начислится миль: " + tiket/bonus);
+    }
+}
